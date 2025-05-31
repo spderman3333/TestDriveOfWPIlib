6caa50323cd1f99e8df126db885a119b3ab61923
vdikov <veselin.dikov@gmail.com>
2025-05-31 12:44:57 -0700

    Reflow some lines of code for better formatting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -69,12 +69,13 @@ public class RobotContainer {
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
                 // Drivetrain will execute this command periodically
-                drivetrain.applyRequest(() -> drive.withVelocityX(joystick.getLeftY() * MaxSpeed) // Drive forward with
-                                                                                                  // negative Y
-                                                                                                  // (forward)
-                        .withVelocityY(joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                        .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with
-                                                                                    // negative X (left)
+                drivetrain.applyRequest( //
+                        () -> drive.withVelocityX(joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y
+                                                                                  // (forward)
+                                .withVelocityY(joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                                .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise
+                                                                                            // with
+                                                                                            // negative X (left)
                 ));
 
         joystick.cross().whileTrue(drivetrain.applyRequest(() -> brake));
